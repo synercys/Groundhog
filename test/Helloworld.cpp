1f3b139f877689f39866f34a7156977122df9d55
disha <agarwal.disha21@gmail.com>
2020-12-06 13:36:43 -0600

    send receive message between multiple machines without port

--- test/Helloworld.cpp
+++ test/Helloworld.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <cryptoTools/Common/config.h>
+
 #include <cryptoTools/Common/Defines.h>
 #include <cryptoTools/Network/Channel.h>
 #include <cryptoTools/Network/Session.h>
@@ -8,56 +11,84 @@
 
 using namespace osuCrypto;
 
-int main(int argc, char** argv)
-{
-    IOService ios(2);
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
 
+std::string getIP() {
+    std::string result = exec("ifconfig");
+    std::istringstream iss(result);
+    std::vector<std::string> tokens{std::istream_iterator<std::string>{iss},
+                                    std::istream_iterator<std::string>{}};
+
+    std::string ip;
+    for(int i=0; i< tokens.size(); i++){
+        if(tokens[i].compare("inet") == 0)
+        {
+            ip = tokens[i+1];
+            break;
+        } 
+    }
+
+    return ip;
+}
+
+void connect(std::vector<std::string> ips, u64 n) {
+    IOService ios(n);
     ios.showErrorMessages(true);
+    std::string ip = getIP();
 
-    auto ip1 = std::string("10.0.60.177");
-    auto ip2 = std::string("10.0.60.64");
-    auto port = 1212;
-
-    // std::string serversIpAddress = ip1 + ':' + std::to_string(port);
-    std::string serversIpAddress = ip1;
-    std::string sessionHint = "party0_party1";
-
-    // Create a pair of sessions that connect to eachother. Note that
-    // the sessionHint parameter is options.
-    Session server(ios, serversIpAddress, SessionMode::Server, sessionHint);
+    int current_node;
     
-    std::string channelName = "channelName";
-    Channel namedChl1 = server.addChannel(channelName);
-
-    std::chrono::milliseconds timeout(1000000000000);
-    namedChl1.waitForConnection();
-
-    namedChl1.onConnect([](const error_code& ec) {
-        if (ec)
-        {
-            std::cout << "chl0 failed to connect: " << ec.message() << std::endl;
+    for(int i = 0; i < n ; i++) {
+        if(ips[i].compare(ip) == 0){
+            current_node = i;    
         }
-        else
+    }
+    
+    for(int i = 0; i < n ; i++) {
+        if(i < current_node) 
         {
-            std::cout << "Channel connected = " << std::endl;
+            //connect as a client  
+            Channel clientChl = Session(ios, ips[i], SessionMode::Client).addChannel();
+            clientChl.send(getIP());
+            std::string msg;
+            clientChl.recv(msg);
+            std::cout << msg << std::endl;
+        } 
+        else if (i == current_node) 
+        {
+            continue;
+        } 
+        else 
+        {  //connect as a server
+            Session perPartySession(ios, ip, SessionMode::Server);
+            Channel serverChl = perPartySession.addChannel();
+            std::chrono::milliseconds timeout(1000000000000);
+            serverChl.waitForConnection();
+            serverChl.send(getIP());
+            std::string msg;
+            serverChl.recv(msg);
+            std::cout << msg << std::endl;
         }
-    });
-
-    
-    std::vector<int> data{ 0,1,2,3,4,5,6,7 };
-    namedChl1.send(data);
-
-    std::vector<int> dest;
-    namedChl1.recv(dest);
-
-    for(int i=0; i < dest.size(); i++)
-        std::cout << dest.at(i) << ' ';
-    
-
-    std::cout << "Channel connected = " << namedChl1.isConnected() << std::endl;
-
-   
+        
+        
+    }
+}
 
 
+int main(int argc, char** argv) {
+    std::vector<std::string> ips { "10.0.60.177", "10.0.60.64", "10.0.60.185"};
+    u64 n = ips.size();
+    connect(ips, n);
 
 }
