f17885b396c303c7ce300e84bc6f9425c8aab600
disha-agarwal <agarwal.disha21@gmail.com>
2020-12-07 14:40:14 -0600

    refactor Helloworld.cpp

--- test/Helloworld.cpp
+++ test/Helloworld.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <cryptoTools/Common/config.h>
 #include <cryptoTools/Common/CLP.h>
-#include <cryptoTools/Common/Defines.h>
-#include <cryptoTools/Network/Channel.h>
-#include <cryptoTools/Network/Session.h>
-#include <cryptoTools/Network/IOService.h>
 #include "util.h"
 
 
@@ -17,9 +9,8 @@ int main(int argc, char** argv) {
     oc::CLP cmd;
     cmd.parse(argc, argv);
 
-    std::vector<std::string> ips { "10.0.60.177", "10.0.60.64", "10.0.60.185"};
+    std::vector<std::string> ips {"10.0.60.64", "10.0.60.177", "10.0.60.185"};
     u64 n = ips.size();
-    // connect(ips, n);
     getLatency(ips, n);
 
 }
