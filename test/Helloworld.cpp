9cf73133ac0851f4fb32edde2d4b411c9b144f3f
disha-agarwal <agarwal.disha21@gmail.com>
2021-05-01 22:30:34 +0000

    update threshold

--- test/Helloworld.cpp
+++ test/Helloworld.cpp
@@ -144,7 +144,7 @@ int main(int argc, char** argv) {
     auto mc = cmd.get<i64>("mc");
 
     auto m = std::max<u64>(2, (mc == -1) ? n * mFrac : mc);
-    m = 4;
+    m = 6;
 
     if (m > n)
     {
