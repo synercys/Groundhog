42213bdc81310b9609132ad7c4f7c0dd72bde66e
disha-agarwal <agarwal.disha21@gmail.com>
2021-05-03 22:13:10 +0000

    threshold update

--- test/Helloworld.cpp
+++ test/Helloworld.cpp
@@ -215,7 +215,7 @@ int main(int argc, char** argv) {
         auto mc = cmd.get<i64>("mc");
 
         auto m = std::max<u64>(2, (mc == -1) ? n * mFrac : mc);
-        m = 12;
+        m = 16;
         std::string stateFileName = "/home/ubuntu/redise/dise/test/seed_file";
 
         if (m > n)
