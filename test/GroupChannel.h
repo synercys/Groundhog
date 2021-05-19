c13cf49c2d0e51f35ae57ea1fd6ebe1114b57e0a
disha-agarwal <agarwal.disha21@gmail.com>
2021-05-19 23:21:57 +0000

    connectiong only to node 0

--- test/GroupChannel.h
+++ test/GroupChannel.h
@@ -17,34 +17,41 @@ class GroupChannel {
         u64 current_node; // index of current node's ip in vector of ips
 
         GroupChannel(std::vector<std::string> ips, u64 n, IOService& ios) {
-            nSessions.resize(n);
+            nSessions.resize(1);
             
             std::string ip = getIP();
             current_node = getCurrNodeIdx(ips, ip);
+
+            nSessions[0].start(ios, ips[0], SessionMode::Client);
+                    
+            Channel clientChl = nSessions[0].addChannel();
+            std::chrono::milliseconds timeout(1000000000000);
+            clientChl.waitForConnection(timeout);
+            nChannels.push_back(clientChl);
             
-            for(int i = 0; i < n ; i++) {
-                if (i < current_node) {
-                    //connect as a client  
-                    nSessions[i].start(ios, ips[i], SessionMode::Client);
+            // for(int i = 0; i < n ; i++) {
+            //     if (i < current_node) {
+            //         //connect as a client  
+            //         nSessions[i].start(ios, ips[i], SessionMode::Client);
                     
-                    Channel clientChl = nSessions[i].addChannel();
-                    std::chrono::milliseconds timeout(1000000000000);
-                    clientChl.waitForConnection(timeout);
-                    nChannels.push_back(clientChl);
-                } else if (i == current_node) {
-                    // Channel dummyChl;
-                    // nChannels.push_back(dummyChl);
-                    continue;
-                } else {   
-                    //connect as a server
-                    nSessions[i].start(ios, ip, SessionMode::Server);
+            //         Channel clientChl = nSessions[i].addChannel();
+            //         std::chrono::milliseconds timeout(1000000000000);
+            //         clientChl.waitForConnection(timeout);
+            //         nChannels.push_back(clientChl);
+            //     } else if (i == current_node) {
+            //         // Channel dummyChl;
+            //         // nChannels.push_back(dummyChl);
+            //         continue;
+            //     } else {   
+            //         //connect as a server
+            //         nSessions[i].start(ios, ip, SessionMode::Server);
                     
-                    Channel serverChl = nSessions[i].addChannel();
-                    std::chrono::milliseconds timeout(1000000000000);
-                    serverChl.waitForConnection(timeout);
-                    nChannels.push_back(serverChl);
-                }
-            }
+            //         Channel serverChl = nSessions[i].addChannel();
+            //         std::chrono::milliseconds timeout(1000000000000);
+            //         serverChl.waitForConnection(timeout);
+            //         nChannels.push_back(serverChl);
+            //     }
+            // }
         }
 
         Channel& getChannel(u64 nodeIdx) {
