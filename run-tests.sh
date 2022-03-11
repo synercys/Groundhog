609b6d0044bfae2d09c717457475139a0ac0cc3e
gabrielkulp <kulpga@oregonstate.edu>
2022-03-10 21:20:39 -0800

    Add build and pull options to test

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NODE_COUNT=20
+NODE_COUNT=10
 THRESHOLD_FRACTION=0.7
 TRIAL_SIZE=4096
 
@@ -8,9 +8,18 @@ export NODE_COUNT
 export THRESHOLD_FRACTION
 export TRIAL_SIZE
 
+if [ "$1" = "--pull" ]; then
+	docker pull gabrielkulp/dise
+	docker pull gabrielkulp/htdise
+fi
+
 # Until this gets addressed, I have to use some ugly hacks to get only the output
 # https://github.com/docker/compose/issues/6026
-docker-compose up --build 2>/dev/null | grep "enc/s:" | cut -d\| -f 2 | sed 's/^[[:space:]]*//'
+if [ "$1" = "--build" ]; then
+	docker-compose up --build
+else
+	docker-compose up 2>/dev/null | grep "enc/s:" | cut -d\| -f 2 | sed 's/^[[:space:]]*//'
+fi
 
 docker run --rm -it gabrielkulp/dise /usr/local/bin/dEncFrontend \
 	-ss -nStart "${NODE_COUNT}" -mf "${THRESHOLD_FRACTION}" -t "${TRIAL_SIZE}" -l
