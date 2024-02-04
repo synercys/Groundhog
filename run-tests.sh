12ba29132a40fa941093c25c9d86eb458e168f25
Ashish Kashinath <akashina@eng.ucsd.edu>
2024-02-04 22:20:04 +0000

    Adding command-line arguments to run-tests.sh to take N and M

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-NODE_COUNT=24
-M=22
+NODE_COUNT=$1
+M=$2
+reviewers_protocol=false
 THRESHOLD_FRACTION=`echo "(($M / $NODE_COUNT))" | bc -l`
 TRIAL_SIZE=100000
 
@@ -10,16 +11,21 @@ export SERVER_COUNT
 export THRESHOLD_FRACTION
 export TRIAL_SIZE
 
-if [ "$1" = "--pull" ]; then
+if [ "$3" = "--pull" ]; then
 	docker pull gabrielkulp/dise
 	docker pull gabrielkulp/htdise
 fi
 
 echo "" > schedule.log
 
-if [ "$1" = "--build" ]; then
+if [ "$3" = "--build" ]; then
 	docker-compose down
 	sleep 5
+	if [ "${M}" = 2 ] && [ "$reviewers_protocol" = true ]; then
+		cp data/reboot_sequence_logs/states_"${NODE_COUNT}"_1_100k.txt data/state.txt
+	else
+		cp data/reboot_sequence_logs/states_"${NODE_COUNT}"_"${M}"_1M.txt data/state.txt
+	fi
 	docker build . -t groundhog:latest || exit 1
 	sleep 5
 	docker-compose up --remove-orphans || exit 2 # show full log
@@ -29,5 +35,5 @@ else
 	docker-compose up 2>/dev/null | grep "enc/s:" | cut -d\| -f 2 | sed 's/^[[:space:]]*//'
 fi
 exit 1
-docker run --privileged --rm -it --security-opt seccomp=/usr/local/default.json gabrielkulp/dise /usr/local/bin/dEncFrontend \
-	-ss -nStart "${NODE_COUNT}" -mf "${THRESHOLD_FRACTION}" -t "${TRIAL_SIZE}" -l
+#docker run --privileged --rm -it --security-opt seccomp=/usr/local/default.json groundhog:latest /usr/local/bin/dEncFrontend 
+#-ss -nStart "${NODE_COUNT}" -mf "${THRESHOLD_FRACTION}" -t "${TRIAL_SIZE}" -l
