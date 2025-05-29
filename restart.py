55c60ca43f31ce26efcee098c7412f549491ee9f
Valeria Grullon <valeriagrulllon@gmail.com>
2025-05-29 14:23:36 -0400

    Code Refactoring

--- restart.py
+++ restart.py
@@ -4,23 +4,23 @@ import subprocess
 import sys
 import socket
 
-exe = "/usr/local/bin/test"
-logfile = "/usr/local/app.log"
-logging.basicConfig(filename=logfile, filemode='w', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+exe = "/usr/local/bin/test" # Path to the program this script runs
+logfile = "/usr/local/app.log" # File where logs will be saved
+logging.basicConfig(filename=logfile, filemode='w', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s') # Set up logging so we can keep track of what the program is doing
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_addr = ('10.0.0.254', 5000) # server IP and port
-proto_dn = b'd' # announce down
-proto_up = b'u' # announce up
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create a socket using the internet protocol (IPv4) and UDP
+server_addr = ('10.0.0.254', 5000)# IP and port of the server that keeps track of which nodes are up or down
+proto_dn = b'd' # Means this node is down
+proto_up = b'u' # Means this node is up
 proto_rq = b'r' # request
 
-def getIP():
+def getIP(): # Get the current computer’s IP address
 	import socket
 	hostname = socket.gethostname()
 	return socket.gethostbyname(hostname)
 
 
-def findNextPrime(n):
+def findNextPrime(n): # Find the next prime number after n (used for math tricks later)
 	if(n <= 1):
 		return 2
 	prime = n
@@ -30,8 +30,9 @@ def findNextPrime(n):
 			break
 	return prime
 
-# TODO: Code needs to be cleaned up
-def isPrime(n):
+
+
+def isPrime(n): # Check if a number is prime (used inside findNextPrime)
 	if (n <= 1):
 		return False 
 	if (n <= 3):
@@ -46,7 +47,7 @@ def isPrime(n):
 	return True
 
 
-def getCurrNodeIdx(ips,ip):
+def getCurrNodeIdx(ips,ip): # Get this node’s index in the list of all node IPs
 	current_node = -1
 	for i in range(len(ips)):
 		if(ips[i] == ip):
@@ -55,22 +56,22 @@ def getCurrNodeIdx(ips,ip):
 	return current_node
 
 
-class RandomNodePicker:
+class RandomNodePicker: # Picks which node should reboot next using modular arithmetic
 	def __init__(self, n):
-		self.n = n
-		self.prime = findNextPrime(n)
-		self.generators = []
+		self.n = n  # Number of nodes
+		self.prime = findNextPrime(n)  # Find a prime number > n
+		self.generators = [] # Store all valid generator sequences
 		for i in range(1,n+1):
 			generatedNums = []
 			generatedNums = self.findGeneratedNums(i,generatedNums)
 			if(len(generatedNums) == n):
-				self.generators.append((i,generatedNums))
+				self.generators.append((i,generatedNums)) # Save the generator and sequence
 				break
-			self.currGeneratorIdx = 0
+			self.currGeneratorIdx = 0 # Start at first generator and first number in sequence
 			self.nextGeneratedNumIdx = 0
 
 
-	def nextNode(self):
+	def nextNode(self): # Return the next node number in the current sequence
 		nodeNum = self.generators[self.currGeneratorIdx][1][self.nextGeneratedNumIdx]
 		self.nextGeneratedNumIdx += 1
 		if(self.nextGeneratedNumIdx == n):
@@ -78,7 +79,7 @@ class RandomNodePicker:
 			self.currGeneratorIdx = (self.currGeneratorIdx + 1)% len(self.generators)
 		return nodeNum
 
-	def findGeneratedNums(self,i,generatedNums):
+	def findGeneratedNums(self,i,generatedNums): # Generate a unique sequence of node numbers using modular math
 		generatedNumsSet = []
 		powersOfiModPrime = 1
 		for x in range(0,self.prime):
@@ -90,45 +91,46 @@ class RandomNodePicker:
 		return generatedNums
 
 
-class Algorithm:
+class Algorithm: # This class runs the logic to determine when to reboot this node
 	def __init__(self,ips, n, attackTime, rebootTime, t, nodePicker):
 		self.ip = getIP()
-		self.mIntervals = max(1,attackTime//rebootTime)
-		self.currNodeIdx = getCurrNodeIdx(ips,self.ip)
-		self.t = t
+		self.mIntervals = max(1,attackTime//rebootTime)  # Total number of reboot periods
+		self.currNodeIdx = getCurrNodeIdx(ips,self.ip) # This node’s position in the network
+		self.t = t # Minimum number of nodes that should always be up
 		self.attackTime = attackTime
 		self.rebootTime = rebootTime
-		self.nodePicker = nodePicker
-		self.n = n 
-		self.numRebootsSoFar = 0
+		self.nodePicker = nodePicker  # How to pick next node to reboot
+		self.n = n  # Total number of nodes
+		self.numRebootsSoFar = 0 # Count of how many times this node has rebooted
 
 
-	def rebootAfterTime(self, timeToReboot):
+	def rebootAfterTime(self, timeToReboot): # Reboot the node after a certain time
 		import time
 		import os
 		self.numRebootsSoFar += 1
-		sock.sendto(proto_up, server_addr)
+		sock.sendto(proto_up, server_addr) # Notify server: this node is going up
 		print("Going up")
 
 		# Reboot logic
 		# timetoReboot is the time after which the node is scheduled to be rebooted. 
-		try:
+		try: # Try to run the test process for timeToReboot seconds
 			# process = subprocess.run("/home/ubuntu/redise/dise/bin/test",universal_newlines=True,capture_output=False,timeout=timeToReboot)
 			process = subprocess.run([exe, "-n", str(server_count)],
 			# process = subprocess.run(["/bin/sleep", "100"],
 					universal_newlines=True, capture_output=True, timeout=timeToReboot)
 			sys.stdout.flush()
 		except subprocess.TimeoutExpired:
-			sock.sendto(proto_dn, server_addr)
+			sock.sendto(proto_dn, server_addr) # Notify server: timeout = going down
 			logging.debug("timeout done")
 			print("timeout done")
 		finally:
-			sock.sendto(proto_dn, server_addr)
-			time.sleep(self.rebootTime)
+			sock.sendto(proto_dn, server_addr)  # Notify server again we are down
+			time.sleep(self.rebootTime) # Wait for reboot time
 		print("Went down")
 
-	def run(self):
+	def run(self): # The logic that decides when this node should reboot
 		if ((self.t) < self.mIntervals):
+			# Simple case: fewer intervals than required nodes
 			subsetSize = self.t
 			# print("here")
 			# print("subset size: ", subsetSize, " mIntervals:",self.mIntervals)
@@ -150,10 +152,11 @@ class Algorithm:
 			# print("timeToReboot: ", timeToReboot) 
 			logging.debug("timeToReboot: " + str(timeToReboot))
 			if(self.numRebootsSoFar>0):
-				timeToReboot += 10
+				timeToReboot += 10  # Extra delay after first reboot
 			self.rebootAfterTime(timeToReboot)
 
 		else:
+			# More intervals than required nodes
 			import math
 			subsetSize = int(math.ceil(self.t/self.mIntervals))
 			# print("subset size: ", subsetSize, " mIntervals:",self.mIntervals)
@@ -178,30 +181,33 @@ class Algorithm:
 			if(self.numRebootsSoFar>0):
 				timeToReboot += 10
 			self.rebootAfterTime(timeToReboot)
-
-
+# === Start of the main program ===
+# Make sure the user gave 4 arguments
 if len(sys.argv) != 1+4:
 	print("Must specify [server count] [threshold fraction] [attack time] [reboot time]")
 	exit(1)
+# Read arguments from command line
+server_count = int(sys.argv[1]) # How many other servers there are
+node_count = server_count + 1  # Include this node
 
-server_count = int(sys.argv[1])
-node_count = server_count + 1
-
+# Create a list of IP addresses for each node
 ips = []
 for i in range(node_count):
 	ips.append("10.0.0." + str(i+2))
 
 # DARPA study 
+# Time and threshold parameters
 attackTime = int(sys.argv[3]) 
 rebootTime = int(sys.argv[4])
-t = int(node_count * float(sys.argv[2]))
+t = int(node_count * float(sys.argv[2]))  # t = number of nodes that must be running at all times
 
+# Initialize node picker and algorithm
 n = node_count
 nodePicker = RandomNodePicker(n)
 # print(nodePicker.generators)
 logging.debug(nodePicker.generators)
 algo = Algorithm(ips,n,attackTime,rebootTime,t,nodePicker)
-while(1):
+while(1): # Keep running the reboot logic forever
 	algo.run()
 
 
